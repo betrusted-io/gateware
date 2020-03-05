2588792102f0a547c194b4cfeaab592da9c886af
bunnie <bunnie@kosagi.com>
2020-03-05 23:46:42 +0800

    try to reduce size of ci.vcd file

--- sim_support/common.v
+++ sim_support/common.v
@@ -20,5 +20,8 @@ end
 
 initial begin
     $dumpfile("ci.vcd");
-    $dumpvars(0, dut);
+    // $dumpvars(0, dut); will dump *everything*
+    $dumpvars(0, success);
+    $dumpvars(0, failure);
+    $dumpvars(0, report);
 end
