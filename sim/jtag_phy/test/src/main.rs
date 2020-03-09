e8d5b510ebebde2a9f8913d0aa6f89058bb84dc9
bunnie <bunnie@kosagi.com>
2020-03-09 19:10:04 +0800

    add JTAG phy simulation
    This is an example of using a native Rust API that includes
    heap allocation (!) to drive a simulation target.

---
+++ sim/jtag_phy/test/src/main.rs
@@ -0,0 +1,75 @@
+#![no_std]
+#![no_main]
+#![feature(alloc_error_handler)]
+
+use sim_bios::sim_test;
+extern crate volatile;
+use volatile::Volatile;
+extern crate jtag;
+use jtag::*;
+
+// pull in external symbols to define heap start and stop
+// defined in memory.x
+extern "C" {
+    static _sheap: u8;
+    static _heap_size: u8;
+}
+
+// Plug in the allocator crate
+#[macro_use]
+extern crate alloc;
+extern crate alloc_riscv;
+
+use alloc_riscv::RiscvHeap;
+
+#[global_allocator]
+static ALLOCATOR: RiscvHeap = RiscvHeap::empty();
+
+// allocate a global, unsafe static string for debug output
+#[used] // This is necessary to keep DBGSTR from being optimized out
+static mut DBGSTR: [u32; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    unsafe{ DBGSTR[0] = layout.size() as u32; }
+    panic!()
+}
+
+const CMD_FUSE_KEY: u32 = 0b110001;
+
+#[sim_test]
+fn run(p: &pac::Peripherals) {
+    unsafe {
+        let heap_start = &_sheap as *const u8 as usize;
+        let heap_size = &_heap_size as *const u8 as usize;
+        ALLOCATOR.init(heap_start, heap_size);
+    }
+
+    let ram_ptr: *mut u32 = 0x0100_0000 as *mut u32;
+    let ram = ram_ptr as *mut Volatile<u32>;
+
+    let mut jm = JtagMach::new();
+    let mut jp = JtagGpioPhy::new();
+
+    jm.reset(&mut jp);
+
+    // get the KEY fuse
+    let mut ir_leg: JtagLeg = JtagLeg::new(JtagChain::IR, "cmd");
+    ir_leg.push_u32(CMD_FUSE_KEY, 6, JtagEndian::Little);
+    jm.add(ir_leg);
+    jm.next(&mut jp);
+
+    let mut ret: u32 = 0;
+    if let Some(mut data) = jm.get() {
+        // it's safe to just pop the "max length" because pop is "best effort only"
+        ret = data.pop_u32(6, JtagEndian::Little).unwrap();
+    }
+
+    // example of updating the "report" bits monitored by the CI framework
+    unsafe {
+        p.SIMSTATUS.report.write(|w| w.bits(ret));
+    }
+
+    // set success to indicate to the CI framework that the test has passed
+    p.SIMSTATUS.simstatus.modify(|_r, w| w.success().bit(true));
+}
---
+++ sim/jtag_phy/top_tb.v
@@ -0,0 +1,45 @@
+`timescale 1ns/1ps
+
+module top_tb();
+
+/////////// boilerplate in here
+`include "common.v"
+
+/////////// DUT code below here
+
+wire tck, tdi, tms;
+reg tdo;
+
+initial begin
+    assign tdo = 1'b1; // just return an 0x3F for now
+end
+
+top dut (
+    // don't touch these two lines
+    .refclk(clk12),
+    .rst(0),
+
+    // dut I/O goes here
+    .jtag_tdi(tdi),
+    .jtag_tdo(tdo),
+    .jtag_tms(tms),
+    .jtag_tck(tck),
+
+    // don't touch these three lines
+    .sim_success(success),
+    .sim_done(done),
+    .sim_report(report)
+);
+
+// add extra variables for CI watching here   
+initial begin
+   $dumpvars(0, tck);
+   $dumpvars(0, tdi);
+   $dumpvars(0, tdo);
+   $dumpvars(0, tms);
+end
+
+// DUT-specific end condition to make sure it eventually stops running for CI mode
+initial #800_000 $finish;
+
+endmodule
