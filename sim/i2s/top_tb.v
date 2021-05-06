1a381e6736f7138ce7c52965ebe9a965333944c4
bunnie <bunnie@kosagi.com>
2021-05-07 05:37:32 +0800

    refine test bench to catch delayed sync case on i2s

--- sim/i2s/top_tb.v
+++ sim/i2s/top_tb.v
@@ -14,7 +14,7 @@ wire sclk;
 reg fpga_reset;
 
 reg mclk;
-initial mclk = 1'b1;
+initial mclk = 1'b0;
 //always #651 mclk = ~mclk;  // 768kHz
 always #65 mclk = ~mclk;  // run 10x faster for faster simulation
 
@@ -22,6 +22,8 @@ reg sync;
 initial sync = 1'b1;
 //always #(1302 * 24) sync = ~sync;  // 24x2 bit sync
 always #(130 * 24) sync = ~sync;  // 24x2 bit sync
+reg sync_d;
+always sync_d = #20 sync;
 
 initial begin
   fpga_reset = 1'b1;  // fpga reset is extra-long to get past init delays of SPINOR; in reality, this is all handled by the config engine
@@ -40,10 +42,10 @@ sim_bench dut (
     .i2s0_clk(mclk),
     .i2s0_tx(tx0),
     .i2s0_rx(tx0),
-    .i2s0_sync(sync),
+    .i2s0_sync(sync_d),
 
     .i2s1_clk(mclk),
-    .i2s1_sync(sync),
+    .i2s1_sync(sync_d),
     .i2s1_tx(tx1),
 
     // don't touch these three lines
@@ -55,7 +57,7 @@ sim_bench dut (
 // add extra variables for CI watching here   
 initial begin
    $dumpvars(0, mclk);
-   $dumpvars(0, sync);
+   $dumpvars(0, sync_d);
    $dumpvars(0, tx0);
    $dumpvars(0, tx1);
 end
