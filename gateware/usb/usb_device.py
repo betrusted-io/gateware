ecf5b98928479be127f8dc1ac8031d564ebb8284
bunnie <bunnie@kosagi.com>
2022-04-21 19:09:18 +0800

    adjust interface to allow for switching between both PHYs

--- gateware/usb/usb_device.py
+++ gateware/usb/usb_device.py
@@ -13,29 +13,18 @@ from litex import get_data_mod
 
 from litex.soc.interconnect import wishbone
 
-from litex.build.io import SDRTristate
+from migen.genlib.cdc import MultiReg
 
 import subprocess, sys
 
 # USB OHCI -----------------------------------------------------------------------------------------
 
 class USBDevice(Module):
-    def __init__(self, platform, pads, usb_clk_freq=48e6, dma_data_width=32):
-        self.pads           = pads
+    def __init__(self, platform, usb_ios, usb_clk_freq=48e6, dma_data_width=32):
         self.usb_clk_freq   = usb_clk_freq
         self.dma_data_width = dma_data_width
 
         self.wb_ctrl = wb_ctrl = wishbone.Interface(data_width=32)
-        self.wb_dma  = wb_dma  = wishbone.Interface(data_width=dma_data_width)
-
-        self.interrupt = Signal()
-
-        # # #
-
-        usb_ios = Record([
-            ("dp_i",  1), ("dp_o",  1), ("dp_oe", 1),
-            ("dm_i",  1), ("dm_o",  1), ("dm_oe", 1),
-        ])
 
         self.specials += Instance(self.get_netlist_name(),
             # Clk / Rst.
@@ -65,23 +54,6 @@ class USBDevice(Module):
             # unsure what this does
             i_io_power = 1,
         )
-        self.specials += SDRTristate(
-            io = pads.d_p,
-            o  = usb_ios.dp_o,
-            oe = usb_ios.dp_oe,
-            i  = usb_ios.dp_i,
-        )
-        self.specials += SDRTristate(
-            io = pads.d_n,
-            o  = usb_ios.dm_o,
-            oe = usb_ios.dm_oe,
-            i  = usb_ios.dm_i,
-        )
-        # k_state is used by the power management system to detect if we should force CPU power on
-        self.k_state = Signal()
-        self.comb += [
-            self.k_state.eq(~usb_ios.dm_i & usb_ios.dp_i)
-        ]
 
         self.add_sources(platform)
 
@@ -105,3 +77,89 @@ class USBDevice(Module):
         subprocess.run('sbt "runMain spinal.lib.com.usb.udc.UsbDeviceCtrlWishboneGen"', cwd=exec_path, shell=True)
 
 
+class IoBuf(Module):
+    def __init__(self, usbp_pin, usbn_pin, alt_ios, alt_sel, usb_pullup_pin=None):
+        reset_duration_in_s = 0.1
+        reset_cycles = int(32768 * reset_duration_in_s)
+        reset_counter = Signal(log2_int(reset_cycles, need_pow2=False)+1, reset=reset_cycles - 1)
+        usb_phy_reset = Signal(reset=1)
+        self.sync.lpclk += \
+            If(reset_counter != 0,
+                reset_counter.eq(reset_counter - 1)
+            ).Else(
+                usb_phy_reset.eq(0)
+            )
+
+        # tx/rx io interface
+        self.usb_tx_en = Signal()
+        self.usb_p_tx = Signal()
+        self.usb_n_tx = Signal()
+
+        self.usb_p_rx = Signal()
+        self.usb_n_rx = Signal()
+
+        usb_p_t = TSTriple()
+        usb_n_t = TSTriple()
+
+        # alt mux
+        mux_dp_o = Signal()
+        mux_dm_o = Signal()
+        mux_dp_oe = Signal()
+        mux_dm_oe = Signal()
+        self.comb += [
+            # inputs directly through
+            alt_ios.dp_i.eq(usb_p_t.i),
+            alt_ios.dm_i.eq(usb_n_t.i),
+            If(alt_sel,
+                mux_dp_o.eq(alt_ios.dp_o),
+                mux_dm_o.eq(alt_ios.dm_o),
+                mux_dp_oe.eq(alt_ios.dp_oe),
+                mux_dm_oe.eq(alt_ios.dm_oe),
+            ).Else(
+                mux_dp_o.eq(self.usb_p_tx),
+                mux_dm_o.eq(self.usb_n_tx),
+                mux_dp_oe.eq(self.usb_tx_en),
+                mux_dm_oe.eq(self.usb_tx_en),
+            )
+        ]
+
+        self.specials += usb_p_t.get_tristate(usbp_pin)
+        self.specials += usb_n_t.get_tristate(usbn_pin)
+
+        self.usb_pullup = Signal()
+        if usb_pullup_pin is not None:
+            self.comb += [
+                usb_pullup_pin.eq(self.usb_pullup),
+            ]
+
+        #######################################################################
+        #######################################################################
+        #### Mux the USB +/- pair with the TX and RX paths
+        #######################################################################
+        #######################################################################
+        usb_p_t_i = Signal()
+        usb_n_t_i = Signal()
+        self.specials += [
+            MultiReg(usb_p_t.i, usb_p_t_i),
+            MultiReg(usb_n_t.i, usb_n_t_i)
+        ]
+        self.comb += [
+            If(self.usb_tx_en,
+                self.usb_p_rx.eq(0b1),
+                self.usb_n_rx.eq(0b0),
+            ).Else(
+                self.usb_p_rx.eq(usb_p_t_i),
+                self.usb_n_rx.eq(usb_n_t_i),
+            ),
+            If(usb_phy_reset,
+                usb_p_t.oe.eq(1),
+                usb_n_t.oe.eq(1),
+                usb_p_t.o.eq(0),
+                usb_n_t.o.eq(0),
+            ).Else(
+                usb_p_t.oe.eq(mux_dp_oe),
+                usb_n_t.oe.eq(mux_dm_oe),
+                usb_p_t.o.eq(mux_dp_o),
+                usb_n_t.o.eq(mux_dm_o),
+            )
+        ]
